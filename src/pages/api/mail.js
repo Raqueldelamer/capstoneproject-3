f344bba913e438c8336fdada84eba6e4ddcfc546
Raquel Marie <raqueldelamer@gmail.com>
2024-11-04 20:58:42 -0600

    update mail.js

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -3,17 +3,26 @@ import { Resend } from 'resend';
 const resend = new Resend('re_g7fTSP5M_ChFbq3sAAgCg3WJc6qHYuFU5');
 
 export default function handler(req, res) {
+    const query = req.query;
+    const name = query.name;
+    const message = query.message;
+    const subject = query.subject;
+
+    console.log(name);
+    console.log(message);
     
-    resend.emails.send ({
+    const email = ({
         
         from: 'onboarding@resend.dev',
         to: 'raqueldelamer@gmail.com',
-        subject: 'Hello World',
-        html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+        subject: `${subject}`,
+        html: `<p><strong>${message}</strong>!</p>`
     
     });
     
-    res.status(200).json({ name: "John Doe" })
+    resend.emails.send(email);
+
+    res.status(200).json({ name: name, message: message })
 
 }
 
