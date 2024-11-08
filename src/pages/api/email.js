bceb269de2ad92dbf2db45bffc3c7526b81dc8aa
Raquel Marie <raqueldelamer@gmail.com>
2024-11-07 23:33:31 -0600

    update email.js

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -6,34 +6,34 @@ const resend = new Resend(RESEND_API_KEY);
 
 export default async function handler(req, res) {
 
-    const query = req.query;
-    const user = query.user;  
-    const subject = req.query.subject; 
-    const message = req.query.message;
-    
+    const { user, subject, message } = req.query;
 
+  //  const query = req.query;
+  //  const user = query.user;  
+  //  const subject = req.query.subject; 
+  //  const message = req.query.message;
     console.log(user);
     console.log(subject);
     console.log(message);
 
 
-    const email = ({
+    const email = {
         
         from: 'onboarding@resend.dev',
         to: 'raqueldelamer@gmail.com',
         subject: `${subject}`,
         html: `${user} says <p><strong>${message}</strong>!</p>`
     
-    });
+    }
     
-    await resend.emails.send(email)
-        .then(() => {
-            res.status(200).json({ message: "Email sent!" });
-    })
-        .catch(error => {
-            console.error("Error sending email:", error);
-            res.status(500).json({ error: 'Failed to send email'});
-        });
+    try {
+        await resend.emails.send(email);
+        res.status(200).json({ message: "Email sent!" });
+    
+    } catch(error) {
+            console.error("Error sending email:", error.response ? error.response.data : error);
+            res.status(500).json({ error: 'Failed to send email', details: error.message });
+        }
     
 
 }
