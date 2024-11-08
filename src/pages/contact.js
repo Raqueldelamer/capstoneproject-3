5662b104885e9223e333d5c3c9d83c390cff541f
Raquel Marie <raqueldelamer@gmail.com>
2024-11-07 22:57:18 -0600

    add id's to input, subject, message form elements

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -39,28 +39,29 @@ export default function MailPage() {
         <div className="bg-black h-screen text-white m-1">
             <h1 className="text-justify text-2xl m-2 font-bold">Contact Me:</h1>
             <form className="ml-2">
-                Name:<input className="px-4 ml-1 mb-1 text-black flex" type="text" value={user} 
+                Name:<input id="user-name" className="px-4 ml-1 mb-1 text-black flex" type="text" value={user} 
                 onChange={changeHandler} /> 
             </form>
 
             <form className="ml-2">
-                Subject: <input className="px-4 ml-1 mb-1 text-black flex" type="text" value={subject} 
+                Subject: <input id="subject" className="px-4 ml-1 mb-1 text-black flex" type="text" value={subject} 
                 onChange={subjectChangeHandler} />
             </form>
             
             <form className="ml-2">
-                Message: <textarea className="border-blue-900 px-4 ml-1 mb-1 text-black flex" value={message} 
+                Message: <textarea id="message" className="border-blue-900 px-4 ml-1 mb-1 text-black flex" value={message} 
                 type="text" onChange={messageChangeHandler} /> 
             </form>
 
             
                 
             <form>
-                <button className="text-white bg-gradient-to-r from-blue-500 via-blue-600
+                <button id="send-message-button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600
                 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
                 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg 
                 text-m px-2 py-2.5 text-center me-2 mt-3 ml-2" 
-                type="button" onClick={handleClick}> :Send Message </button>
+                type="button" onClick={handleClick}> :Send Message 
+                </button>
             </form>
         </div>
     );
