01e19cc2b85bc17f11e4eee4075263411242e4fb
Raquel Marie <raqueldelamer@gmail.com>
2024-11-10 13:15:40 -0600

    update email.js & contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -52,26 +52,21 @@ export default function MailPage() {
             <form className="ml-2">
                 Name:<input id="user-name" className="px-4 ml-1 mb-1 text-black flex" type="text" value={user} 
                 onChange={changeHandler} /> 
-            </form>
 
-            <form className="ml-2">
                 Subject: <input id="subject" className="px-4 ml-1 mb-1 text-black flex" type="text" value={subject} 
                 onChange={subjectChangeHandler} />
-            </form>
             
-            <form className="ml-2">
+            
                 Message: <textarea id="message" className="border-blue-900 px-4 ml-1 mb-1 text-black flex" value={message} 
                 type="text" onChange={messageChangeHandler} /> 
             </form>
 
-            
-                
             <form>
                 <button id="send-message-button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600
                 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none
                 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg 
                 text-m px-2 py-2.5 text-center me-2 mt-3 ml-2" 
-                type="button" onClick={handleClick}> :Send Message 
+                type="button" onClick={handleClick}> Send Message 
                 </button>
             </form>
             {/* Feedback message */}
